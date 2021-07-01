e9e8b185db490bb5d4943ad4da2a8f14b5860f6d
Jakub Podoba <51724203+jpodoba@users.noreply.github.com>
2021-07-01 17:18:39 +0200

    Create k8s-install-ubuntu.sh

---
+++ k8s-install-ubuntu.sh
@@ -0,0 +1,42 @@
+swapoff -a
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sudo sysctl --system
+
+sudo apt-get update 
+sudo apt-get install -y containerd
+
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+
+sudo sed -i 's/.containerd.runtimes.runc.options]/.containerd.runtimes.runc.options]\n            SystemdCgroup = true/' /etc/containerd/config.toml
+
+sudo systemctl restart containerd
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+sudo bash -c 'cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF'
+
+sudo apt-get update
+
+VERSION=1.21.2-00
+sudo apt-get install -y kubelet=$VERSION kubeadm=$VERSION kubectl=$VERSION
+sudo apt-mark hold kubelet kubeadm kubectl containerd
+
+sudo systemctl enable kubelet.service
+sudo systemctl enable containerd.service
